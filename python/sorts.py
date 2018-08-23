62c609a15aaee9969b5527aa35e9f5b8039a42ca
nrsyed <nsyed88@gmail.com>
2018-08-23 01:41:40 -0400

    Add comments to sorts.py

--- python/sorts.py
+++ python/sorts.py
@@ -115,15 +115,19 @@ def selectionsort(A):
         yield A
 
 if __name__ == "__main__":
+    # Get user input to determine range of integers (1 to N) and desired
+    # sorting method (algorithm).
     N = int(input("Enter number of integers: "))
     method_msg = "Enter sorting method:\n(b)ubble\n(i)nsertion\n(m)erge \
         \n(q)uick\n(s)election\n"
     method = input(method_msg)
 
+    # Build and randomly shuffle list of integers.
     A = [x + 1 for x in range(N)]
     random.seed(time.time())
     random.shuffle(A)
 
+    # Get appropriate generator to supply to matplotlib FuncAnimation method.
     if method == "b":
         title = "Bubble sort"
         generator = bubblesort(A)
@@ -140,13 +144,35 @@ if __name__ == "__main__":
         title = "Selection sort"
         generator = selectionsort(A)
 
+    # Initialize figure and axis.
     fig, ax = plt.subplots()
+    ax.set_title(title)
+
+    # Initialize a bar plot. Note that matplotlib.pyplot.bar() returns a
+    # list of rectangles (with each bar in the bar plot corresponding
+    # to one rectangle), which we store in bar_rects.
     bar_rects = ax.bar(range(len(A)), A, align="edge")
+
+    # Set axis limits. Set y axis upper limit high enough that the tops of
+    # the bars won't overlap with the text label.
     ax.set_xlim(0, N)
     ax.set_ylim(0, int(1.07 * N))
-    ax.set_title(title)
+
+    # Place a text label in the upper-left corner of the plot to display
+    # number of operations performed by the sorting algorithm (each "yield"
+    # is treated as 1 operation).
     text = ax.text(0.02, 0.95, "", transform=ax.transAxes)
 
+    # Define function update_fig() for use with matplotlib.pyplot.FuncAnimation().
+    # To track the number of operations, i.e., iterations through which the
+    # animation has gone, define a variable "iteration". This variable will
+    # be passed to update_fig() to update the text label, and will also be
+    # incremented in update_fig(). For this increment to be reflected outside
+    # the function, we make "iteration" a list of 1 element, since lists (and
+    # other mutable objects) are passed by reference (but an integer would be
+    # passed by value).
+    # NOTE: Alternatively, iteration could be re-declared within update_fig()
+    # with the "global" keyword (or "nonlocal" keyword).
     iteration = [0]
     def update_fig(A, rects, iteration):
         for rect, val in zip(rects, A):
